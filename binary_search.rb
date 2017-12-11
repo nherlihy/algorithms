c9473c22264001096da5cd2cbcac1b9304011fa8
Nick Herlihy <nherlihy@sessionm.com>
2017-12-11 15:46:06 -0500

    Cleaning up code

--- binary_search.rb
+++ binary_search.rb
@@ -2,14 +2,16 @@
 # O(Log n)
 
 def search(list, num)
+	puts "Using array:\n#{list}"
+	puts "Searching for #{num}"
 	min = 0
 	max = list.size - 1
 	
 	match = false
 
 	while !match
+		# Number is not in the array
 		if max < min
-			puts "Number is not in the array"
 			break
 		end
 
@@ -17,21 +19,17 @@ def search(list, num)
 		puts "Searching at index: #{index_search}"
 
 		if list[index_search] == num
-			puts "is equal"
 			match = true
+			puts "Found #{num} at index #{index_search}"
 
 		elsif list[index_search] > num
-			puts "Is greater"
 			max = index_search - 1
 
 		else
-			puts "Is less"
 			min = index_search + 1
 		end
-
-		puts "Max: #{max}\n Min: #{min}\n\n"
 	end
 end
 
 test = [1,2,3,4,5,6,7,8,9,10]
-search(test,10)
+search(test,3)
