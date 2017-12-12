5bb85b1f365b4668fb28f9b1935168cf68d03779
Nick Herlihy <nherlihy@sessionm.com>
2017-12-12 15:56:55 -0500

    Completing selection sort algorithm

--- selection_sort.rb
+++ selection_sort.rb
@@ -1,9 +1,13 @@
+# Sorts list using selection sort. Theres no good or bad case
+# O(n^2)
+
 
 def sort(list)
-	(0 ..(list.length -1)).each do |index|
+    length = list.length - 1
+	(0..length).each do |index|
 		min = index
-
-		(index+1 ..(index.length -1)).each do |a|
+        
+		(index+1 ..length).each do |a|
 			if list[a] < list[min]
 				min = a
 			end
@@ -16,7 +20,7 @@ def sort(list)
 	list
 end
 
-list = [6,2,4,7]
+list = [6,2,4,7,100,3,6,7,8,25]
 sorted_list = sort(list)
 puts sorted_list
 
